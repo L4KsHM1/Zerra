d38fad2aefbab50157f281cb800319e3181f75f0
L4KsHM1 <lakshmishajan.me@gmail.com>
2023-10-01 18:27:57 +0530

    js loop

--- main.js
+++ main.js
@@ -1,19 +1,10 @@
 import "./style.css";
-const imgContainer = document.querySelector(".gallery");
+const galleryContainer = document.querySelector(".gallery");
 const blogContainer = document.querySelector(".blog-img");
-const imgArray = [
-  {
-    imgUrl:
-      "https://images.unsplash.com/photo-1584280795027-321f4d68e77b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
-  },
-  {
-    imgUrl:
-      "https://images.unsplash.com/photo-1589834390005-5d4fb9bf3d32?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
-  },
-  {
-    imgUrl:
-      "https://images.unsplash.com/photo-1527005980469-e172416c200b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
-  },
+const galleryImages = [
+  "https://images.unsplash.com/photo-1584280795027-321f4d68e77b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
+  "https://images.unsplash.com/photo-1589834390005-5d4fb9bf3d32?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
+  "https://images.unsplash.com/photo-1527005980469-e172416c200b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
 ];
 
 const blogArray = [
@@ -28,30 +19,30 @@ const blogArray = [
     date: "12 May 2023",
   },
   {
-    img: "https://images.unsplash.com/photo-1580785692886-839ae39fc37c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2073&q=80",
+    img: "https://images.unsplash.com/photo-1559523275-98fb3c56faf6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2013&q=80",
     text: "Webinar event Mental training for young people to develop",
     date: "15 Apr 2023",
   },
 ];
 
 blogArray.forEach((blog) => {
-  let blogHtml = `<div class="section"
-  <img
-    src=" ${blog.img}"
+  let blogHtml = `<div class="section">
+    <img
+    src="${blog.img}"
     alt=""
-  />
+    />
   <p>${blog.text}</p>
   <h4>${blog.date}</h4>
 </div>`;
   blogContainer.innerHTML += blogHtml;
 });
 
-imgArray.forEach((url) => {
-  let imgHtml = `<div class="img1">
+galleryImages.forEach((url) => {
+  let imgHtml = `<div class="">
   <img
-    src="${url.imgUrl}"
+    src="${url}"
     alt=""
   />
 </div>`;
-  imgContainer.innerHTML += imgHtml;
+  galleryContainer.innerHTML += imgHtml;
 });
